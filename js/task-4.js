5286312644345d12af34f859c4019bd3530f7aee
pavlo <pashapasha600@gmail.com>
2024-02-25 20:03:26 +0100

    hw5

--- js/task-4.js
+++ js/task-4.js
@@ -24,3 +24,52 @@
 // console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
 // console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
 // console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+const getTotalBalanceByGender = (users, gender) => {
+    return users
+        
+        .filter(user => user.gender === gender)
+        .reduce((totaleBalance, user) => totaleBalance + user.balance, 0);
+  
+};
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
